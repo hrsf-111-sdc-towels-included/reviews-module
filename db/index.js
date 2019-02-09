f2861c54ad5fa91aa287f7272535a76eb898ee3a
Lukas Singer <lukassinger@Lukass-MacBook-Pro.local>
2019-02-08 19:12:13 -0800

    Set up database for seeding chart

--- db/index.js
+++ db/index.js
@@ -3,4 +3,22 @@ const mysqlConfig = require('./config.js');
 
 const connection = mysql.createConnection(mysqlConfig);
 
-module.exports.connection = connection;
+const getReviews = function(serverCallback) {
+    let query = 'SELECT * FROM Reviews';
+    let sqlCb = (err, dbData) => {
+      if (err) {
+        throw err;
+        return
+      } else {
+        console.log('dbdata', dbData)
+        serverCallback(null, dbData)
+      }
+    }
+    connection.query(query, sqlCb)
+  }
+
+
+
+module.exports = {
+    getReviews
+};
