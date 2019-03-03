8c4b382b46993656871131abb75610a94c1f1c87
John Castberg <jpcastberg@gmail.com>
2019-03-02 21:31:21 -0800

    Refactor Riak seeding script

---
+++ db/sdc/helpers.js
@@ -0,0 +1,29 @@
+const faker = require('faker');
+const moment = require('moment');
+
+module.exports.reviewsGenerator = (current) => {
+  const reviews = [];
+  const amountOfReviews = faker.random.number({ min: 10, max: 20 });
+  for (let i = 0; i < amountOfReviews; i += 1) {
+    reviews.push({
+      homeID: current,
+      username: faker.image.avatar().slice(47, -8),
+      accuracy: faker.random.number({ min: 1, max: 5 }),
+      communication: faker.random.number({ min: 1, max: 5 }),
+      cleanliness: faker.random.number({ min: 1, max: 5 }),
+      location: faker.random.number({ min: 1, max: 5 }),
+      checkIn: faker.random.number({ min: 1, max: 5 }),
+      value: faker.random.number({ min: 1, max: 5 }),
+      complaints: faker.random.number({ min: 0, max: 1 }),
+      comment: faker.lorem.sentences(),
+      createdAt: moment(faker.date.past(2, new Date())).format('YYYY-MM-DD'),
+    });
+  }
+  return reviews;
+};
+
+module.exports.validateBatch = (totalToWrite, batchSize) => {
+  if (totalToWrite % batchSize !== 0) {
+    throw new Error(`Invalid batch size! ${totalToWrite}%${batchSize} === ${totalToWrite % batchSize}`);
+  }
+};
--- db/sdc/riakSeeding.js
+++ db/sdc/riakSeeding.js
@@ -1,160 +1,70 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable no-loop-func */
 const async = require('async');
-// const riakClient = require('./index.js');
 
-// riakClient.storeValue({
-//   bucket: 'this-is-a-test-bucket',
-//   key: 'abcd',
-//   value: [{ a: 1 }, { b: 2 }],
-// }, (err) => {
-//   if (err) throw new Error(err);
-//   console.log('Wrote abcd');
-//   riakClient.fetchValue({
-//     bucket: 'this-is-a-test-bucket',
-//     key: 'abcd',
-//     convertToJs: true,
-//   }, (err, rslt) => {
-//     if (err) throw new Error(err);
-//     console.log('And here it is:', JSON.stringify(rslt.values[0].value));
-//   });
-// });
+const riakClient = require('./index.js');
+const helpers = require('./helpers.js');
 
-/* Inputs: Data generator, amount of records written,
-* total amount of records to write, callback
-* *
-* Outputs: confirmation that each set of records was written
-* final confirmation that the set is written
-*/
-
-const validateBatch = (batchSize, totalToWrite) => {
-  if (totalToWrite % batchSize !== 0) {
-    throw new Error(`Invalid batch size! ${totalToWrite}%${batchSize} === ${totalToWrite % batchSize}`);
+const writeToRiakNTimes = (writeOptions, callback) => {
+  if (!writeOptions.currentValue) {
+    writeOptions.currentValue = 1;
   }
-};
-
-// writeToRiakNTimes((startingValue) => {
-//   return {
-//     thisIsTheStartingValue: startingValue,
-//   };
-// }, 1, 10, 100, (err) => {
-//   if (err) throw new Error(err);
-//   console.log('Operation Finished!');
-// });
-
-const writeToRiakNTimes = (dataGenerator, startingValue, batchSize, totalToWrite, finalCallback) => {
-  validateBatch(batchSize, totalToWrite);
-  const currentBatch = startingValue + (batchSize - 1);
+  let { currentValue } = writeOptions;
+  const { dataGenerator, bucket, batchSize, totalToWrite } = writeOptions;
+  const currentBatch = currentValue + (batchSize - 1);
   const asyncFunctions = [];
-  while (startingValue <= currentBatch && startingValue <= totalToWrite) {
-    for (startingValue; startingValue <= currentBatch; startingValue += 1) ((startingValue) => {
-      const bucket = 'this-is-a-test-bucket';
-      const key = startingValue.toString();
-      const value = dataGenerator(startingValue);
-      const options = {
-        bucket,
-        key,
-        value,
-      };
-      asyncFunctions.push((asyncCB) => {
-        setTimeout(() => {
-          asyncCB(null);
-        }, 0);
-      });
-    })(startingValue);
-    if (startingValue !== totalToWrite + 1) {
-      async.parallel(asyncFunctions, (err) => {
-        if (err) throw new Error(err);
-        writeToRiakNTimes(dataGenerator, startingValue, batchSize, totalToWrite, finalCallback);
-      });
-    } else {
-      // Last write!!
-      async.parallel(asyncFunctions, finalCallback);
-    }
+
+  // Validate that batch size % total to write === 0
+  helpers.validateBatch(totalToWrite, batchSize);
+
+  // Used IIFE here to maintain unique scopes for each iteration
+  for (currentValue; currentValue <= currentBatch; currentValue += 1) ((currentValue) => {
+    const key = currentValue.toString();
+    const value = dataGenerator(currentValue); // { thisIsATest: '1626' };
+    const options = { bucket, key, value };
+
+    asyncFunctions.push((asyncCB) => {
+      riakClient.storeValue(options, err => asyncCB(err));
+      // setTimeout(() => {
+      //   console.log(JSON.stringify(options.value));
+      //   asyncCB(null);
+      // });
+    });
+  })(currentValue);
+
+  // Loop will eventually set current value to totalToWrite plus one
+  if (currentValue !== totalToWrite + 1) {
+    async.parallel(asyncFunctions, (err) => {
+      if (err) throw new Error(err);
+      // Reset the current value on writeOptions
+      writeOptions.currentValue = currentValue;
+      writeToRiakNTimes(writeOptions, callback);
+    });
+  } else {
+    // Last write!!
+    async.parallel(asyncFunctions, callback);
   }
 };
 
-writeToRiakNTimes((startingValue) => {
-  return {
-    thisIsTheStartingValue: startingValue,
-  };
-}, 1, 10, 20, (err) => {
+const start = new Date();
+writeToRiakNTimes({
+  dataGenerator: helpers.reviewsGenerator,
+  bucket: 'this-is-a-test-bucket',
+  batchSize: 10,
+  totalToWrite: 100,
+}, (err) => {
   if (err) throw new Error(err);
-  console.log('Operation Finished!', writeCounter);
+  const end = new Date();
+  console.log(`Operation Finished! Took ${end - start}ms.`);
 });
 
-// function writeNTimes(n, dataGenerator, writer, encoding, callback) {
-//   const startTimer = new Date();
-//   let i = n + 1;
-//   let data;
-//   const write = () => {
-//     let ok = true;
-//     do {
-//       data = dataGenerator(i);
-//       if (i === n + 1) {
-//         data = dataGenerator.schema;
-//         writer.write(data, encoding);
-//       } else if (i === 1) {
-//         // last time!
-//         writer.write(data, encoding, callback);
-//         const endTimer = new Date();
-//         console.log(`Operation completed in ${endTimer - startTimer}ms`);
-//       } else {
-//         // see if we should continue, or wait
-//         // don't pass the callback, because we're not done yet.
-//         ok = writer.write(data, encoding);
-//       }
-//       i -= 1;
-//     } while (i > 0 && ok);
-//     if (i > 0) {
-//       // had to stop early!
-//       // write some more once it drains
-//       writer.once('drain', write);
-//     }
-//   };
-//   write();
-// }
-
-// for (let i = 0; i < 100; i++) {
-//   asyncFunctions.push(() => {
-//     riakClient.storeValue({
-//       bucket: 'this-is-a-test-bucket',
-//       key: i.toString(),
-//       value: {
-//         value: `!!!!hey ${i} this is a test ${i} object!!!!`,
-//       },
-//     }, (err) => {
-//       if (err) throw new Error(err);
-//       console.log(`Wrote ${i}`);
-//     });
-//   });
-// }
-
-// async.parallel(asyncFunctions, (err, rslts) => {
-//   if (err) {
-//     throw new Error(err);
-//   } else {
-//     console.log('[TasteOfRiakIntro] people stored in Riak');
-//   }
-// });
-
-// for (let i = 0; i < 10; i++) {
-//   asyncFunctions.push(() => {
-//     riakClient.fetchValue({
-//       bucket: 'this-is-a-test-bucket',
-//       key: i.toString(),
-//       convertToJs: true,
-//     },
-//     (err, rslt) => {
-//       if (err) return new Error(err);
-//       console.log(JSON.stringify(rslt.values[0].value));
-//     });
-//   });
-// }
-
-// async.parallel(asyncFunctions, (err, rslts) => {
-//   if (err) {
-//     throw new Error(err);
-//   } else {
-//     console.log('[TasteOfRiakIntro] people stored in Riak');
-//   }
+// riakClient.fetchValue({
+//   bucket: 'this-is-a-test-bucket',
+//   key: '10',
+//   convertToJs: true,
+// }, (err, data) => {
+//   if (err) return new Error(err);
+//   const end = new Date();
+//   console.log(JSON.stringify(data.values[0].value));
+//   console.log(`Operation Finished! Took ${end - start}ms.`);
 // });
