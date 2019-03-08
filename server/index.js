99521c84de4e3243418bf519d0d95dd602cf2582
John Castberg <jpcastberg@gmail.com>
2019-03-07 22:08:13 -0800

    Implement CREATE and READ request handlers in server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const db = require('../db/index.js');
-const riakConnection = require('../db/sdc/index.js');
+const riakConnection = require('../db/riak/index.js');
 
 const app = express();
 const Port = 3004;
@@ -18,61 +17,17 @@ app.use(function(req, res, next) {
 
 app.use(express.static(path.join(__dirname, '/../client/dist')));
 
-// app.get('/api/reviews/:homeId', (req, res) => {
-//   db.getReviews(req.params.homeId, (err, reviews) => {
-//     if (err) {
-//       console.log("We have a server problem", err);
-//     } else {
-//       res.send(reviews);
-//       res.end();
-//     }
-//   });
-// });
-
-// app.get('/api/author/:homeId', (req, res) => {
-//   db.getAuthors((err, authors) => {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       res.send(authors);
-//       res.end();
-//     }
-//   });
-// });
-
 //
 // ─── CRUD API ───────────────────────────────────────────────────────────────────
 //
 
-app.route('/reviews/:itemID')
+app.route('/reviews/:homeID')
 
   // Create
-  .post((req, res) => {
-    const { itemID } = req.params;
-    const reviewToPost = req.body;
-    console.log(`POST req received -- item id is ${itemID}, review to post is ${JSON.stringify(reviewToPost)}`);
-    const handleDBResponse = (err, data) => {
-      if (err) res.status(400).end();
-      res.status(201)
-        .send(data)
-        .end();
-    };
-    riakConnection.storeValue();
-    // send itemID, reviewToPost and handleDBResponse to the database
-  })
+  .post(riakConnection.postReview)
 
   // Read
-  .get((req, res) => {
-    const { itemID } = req.params;
-    console.log(`GET req received -- item id is ${itemID}`);
-    const handleDBResponse = (err, data) => {
-      if (err) res.status(404).end();
-      res.status(200)
-        .send(data)
-        .end();
-    };
-    // send itemID, and handleDBResponse to the database
-  })
+  .get(riakConnection.getReviews)
 
   // Update
   .put((req, res) => {
